c40ca00eb20e3685e9205c54c325f137dbee2fc7
Paolo Montano - Laptop <Paolo.Montano@Gmail.com>
2015-02-15 20:17:11 -0800

    Initial commit.

---
+++ MVC01/Models/NwPartials/Product.cs
@@ -0,0 +1,36 @@
+﻿using MVC01.Models.CustomValidation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC01.Models.Northwind
+{
+    [MetadataType(typeof(ProductMetaData))]
+    public partial class Product { }
+
+    public class ProductMetaData
+    {
+        [HiddenInput(DisplayValue = false)]
+        public object ProductID { get; set; }
+
+        [Required (ErrorMessage="Product name is required.")]
+        [Display(Name = "Product Name")]
+        [StringLength(30, ErrorMessage = "The {0} must be between {2} and {1} characters.", MinimumLength = 5)]
+        [ExcludeChar("/.,!@#$%", ErrorMessage = "Name contains invalid character.")]
+        public object ProductName { get; set; }
+
+        [Required(ErrorMessage = "Unit price is required.")]
+        [DataType(DataType.Currency)]
+        public object UnitPrice { get; set; }
+
+        [ScaffoldColumn(false)]
+        public object ReorderLevel { get; set; }
+
+        [UIHint("CategoryDropDown")]
+        public object CategoryID { get; set; }
+
+    }
+}
